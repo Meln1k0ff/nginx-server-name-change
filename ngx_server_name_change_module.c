f3213ec1583aa68c1260daf9045c6d5ff6710107
Pavel-Melnikov <p.melnikov101@gmail.com>
2017-11-05 23:44:03 -0800

    First attempt to initiate a module - Cant see a debug log

--- ngx_server_name_change_module.c
+++ ngx_server_name_change_module.c
@@ -1,5 +1,5 @@
 /**
- * @file   ngx_http_hello_world_module.c
+ * @file   ngx_server_name_change_module.c
  * @author António P. P. Almeida <appa@perusio.net>
  * @date   Wed Aug 17 12:06:52 2011
  *
@@ -29,37 +29,39 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
-
+#include <ngx_log.h>
 
 #define SERVER_NAME_CHANGE "server_name_change"
 
-static char *ngx_http_hello_world(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
-static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t *r);
-
+static ngx_int_t ngx_server_modify_hostname_init(ngx_conf_t *cf);
+static char *ngx_server_name_change(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+//static ngx_int_t ngx_server_name_change_handler(ngx_http_request_t *r);
+static ngx_int_t ngx_server_modify_hostname(ngx_http_request_t *r);
 /**
- * This module provided directive: hello world.
+ * This module provided directive: server_name_change
  *
  */
-static ngx_command_t ngx_http_hello_world_commands[] = {
+static ngx_command_t ngx_server_name_change_commands[] = {
 
     { ngx_string("server_name_change"), /* directive */
-      NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS, /* location context and takes
-                                            no arguments*/
-      ngx_http_hello_world, /* configuration setup function */
+      NGX_HTTP_LOC_CONF|NGX_CONF_TAKE2, /* location context and takes
+                                            2 arguments*/
+      ngx_server_name_change, /* configuration setup function */
       0, /* No offset. Only one context is supported. */
       0, /* No offset when storing the module configuration on struct. */
-      NULL},
+      NULL
+    },
 
     ngx_null_command /* command termination */
 };
 
-/* The hello world string. */
-static u_char ngx_hello_world[] = HELLO_WORLD;
+/* The server_name_changed with args string. Should get the total LEN with args */
+static u_char ngx_server_name_changed[] = SERVER_NAME_CHANGE;
 
 /* The module context. */
-static ngx_http_module_t ngx_http_hello_world_module_ctx = {
+static ngx_http_module_t ngx_server_name_change_module_ctx = {
     NULL, /* preconfiguration */
-    NULL, /* postconfiguration */
+    ngx_server_modify_hostname_init, /* postconfiguration */
 
     NULL, /* create main configuration */
     NULL, /* init main configuration */
@@ -72,10 +74,10 @@ static ngx_http_module_t ngx_http_hello_world_module_ctx = {
 };
 
 /* Module definition. */
-ngx_module_t ngx_http_hello_world_module = {
+ngx_module_t ngx_server_name_change_module = {
     NGX_MODULE_V1,
-    &ngx_http_hello_world_module_ctx, /* module context */
-    ngx_http_hello_world_commands, /* module directives */
+    &ngx_server_name_change_module_ctx, /* module context */
+    ngx_server_name_change_commands, /* module directives */
     NGX_HTTP_MODULE, /* module type */
     NULL, /* init master */
     NULL, /* init module */
@@ -95,10 +97,20 @@ ngx_module_t ngx_http_hello_world_module = {
  * @return
  *   The status of the response generation.
  */
-static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t *r)
+
+static ngx_int_t ngx_server_modify_hostname_init(ngx_conf_t *cf){
+    ngx_log_t *log = ngx_log_init("");
+    const char *init= "init";
+    ngx_log_debug(NGX_LOG_DEBUG,cf->log,"Init succesfully %s",init);
+    ngx_log_debug(NGX_LOG_DEBUG,log,"Init succesfully %s",init);
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_server_modify_hostname(ngx_http_request_t *r)
 {
     ngx_buf_t *b;
     ngx_chain_t out;
+    ngx_log_t *log = ngx_log_init("");
 
     /* Set the Content-Type header. */
     r->headers_out.content_type.len = sizeof("text/plain") - 1;
@@ -106,25 +118,37 @@ static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t *r)
 
     /* Allocate a new buffer for sending out the reply. */
     b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+    ngx_http_parse_request_line(r,b);
+
+    //u_char *host = r->host_start;
+//    char *host = malloc(strlen((const char*)host_end));
+//    memcpy(host,host_end,strlen(host));
+//    ngx_log_debug(1,host_end);
+    const char *l = "localhost";
+     ngx_log_debug(NGX_LOG_DEBUG,log,"rmofrfrformformformfromfromfromfrofmrmfromfromfmrofmrmfrofmrofmrofmromfrmofrmformPos = %s",l);
+     r->host_start = (u_char)l;
+
 
     /* Insertion in the buffer chain. */
     out.buf = b;
     out.next = NULL; /* just one buffer */
 
-    b->pos = ngx_hello_world; /* first position in memory of the data */
-    b->last = ngx_hello_world + sizeof(ngx_hello_world); /* last position in memory of the data */
+    b->pos = ngx_server_name_changed; /* first position in memory of the data */
+
+    b->last = ngx_server_name_changed + sizeof(ngx_server_name_changed); /* last position in memory of the data */
     b->memory = 1; /* content is in read-only memory */
     b->last_buf = 1; /* there will be no more buffers in the request */
 
-    /* Sending the headers for the reply. */
+//    /* Sending the headers for the reply. */
     r->headers_out.status = NGX_HTTP_OK; /* 200 status code */
-    /* Get the content length of the body. */
-    r->headers_out.content_length_n = sizeof(ngx_hello_world);
+//    /* Get the content length of the body. */
+    r->headers_out.content_length_n = sizeof(ngx_server_name_changed);
     ngx_http_send_header(r); /* Send the headers */
 
     /* Send the body, and return the status code of the output filter chain. */
     return ngx_http_output_filter(r, &out);
-} /* ngx_http_hello_world_handler */
+} /* ngx_server_name_change_handler */
+
 
 /**
  * Configuration setup function that installs the content handler.
@@ -138,13 +162,13 @@ static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t *r)
  * @return string
  *   Status of the configuration setup.
  */
-static char *ngx_http_hello_world(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+static char *ngx_server_name_change(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
     ngx_http_core_loc_conf_t *clcf; /* pointer to core location configuration */
 
     /* Install the hello world handler. */
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-    clcf->handler = ngx_http_hello_world_handler;
+    clcf->handler = ngx_server_modify_hostname;
 
     return NGX_CONF_OK;
-} /* ngx_http_hello_world */
+} /* ngx_server_name_change */
